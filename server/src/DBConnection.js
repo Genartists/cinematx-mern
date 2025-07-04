b71cb6a9921d5b183ca73ffa85e5ff9550be5880
Harry Le <n01605830@humbermail.ca>
2025-07-03 22:39:27 -0400

    Update DBConnection.js

--- server/src/DBConnection.js
+++ server/src/DBConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/moviedb");
+    await mongoose.connect("{URL}");
     console.log("mongodb connected!");
   } catch (err) {
     console.error(err.message);
