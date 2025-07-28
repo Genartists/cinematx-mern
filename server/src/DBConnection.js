f05056da6262834a262b9f131c380ddf06d5090b
Hao <n01605830@humbermail.ca>
2025-07-28 15:01:46 -0400

    update fetures

--- server/src/DBConnection.js
+++ server/src/DBConnection.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+<<<<<<< HEAD
     await mongoose.connect("{URL}");
+=======
+    await mongoose.connect("mongodb://localhost:27017/moviedb");
+>>>>>>> c584d35 (First Commit# Please enter the commit message for your changes. Lines starting)
     console.log("mongodb connected!");
   } catch (err) {
     console.error(err.message);
