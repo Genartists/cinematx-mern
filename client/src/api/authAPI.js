d5bd5291a3416f153bb568e7709e7c877388d49c
Hao <n01605830@humbermail.ca>
2025-08-04 22:40:56 -0400

    vite env fix

--- client/src/api/authAPI.js
+++ client/src/api/authAPI.js
@@ -1,14 +1,15 @@
 import axios from "axios";
+import.meta.env.VITE_API_URL;
 
-const API_URL = "http://localhost:5000/api/auth";
+const BASE_URL = import.meta.env.VITE_API_URL + "/api/auth";
 
 export async function registerUser({ username, password }) {
-  const res = await axios.post(`${API_URL}/register`, { username, password });
+  const res = await axios.post(`${BASE_URL}/register`, { username, password });
   return res.data;
 }
 
 export async function loginUser({ username, password }) {
-  const res = await axios.post(`${API_URL}/login`, { username, password });
+  const res = await axios.post(`${BASE_URL}/login`, { username, password });
   localStorage.setItem("token", res.data.data.token);
   return res.data;
 }
--- client/src/api/moviesAPI.js
+++ client/src/api/moviesAPI.js
@@ -1,10 +1,11 @@
 import axios from "axios";
+import.meta.env.VITE_API_URL;
 
-const API_URL = "http://localhost:5000/api/movies";
+const BASE_URL = import.meta.env.VITE_API_URL + "/api/movies";
 
 export async function fetchMovies() {
   const token = localStorage.getItem("token");
-  const res = await axios.get(`${API_URL}/mine`, {
+  const res = await axios.get(`${BASE_URL}/mine`, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res.data.data;
@@ -12,7 +13,7 @@ export async function fetchMovies() {
 
 export async function createMovie(movie) {
   const token = localStorage.getItem("token");
-  const res = await axios.post(API_URL, movie, {
+  const res = await axios.post(BASE_URL, movie, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res.data;
@@ -20,7 +21,7 @@ export async function createMovie(movie) {
 
 export async function deleteMovie(id) {
   const token = localStorage.getItem("token");
-  const res = await axios.delete(`${API_URL}/${id}`, {
+  const res = await axios.delete(`${BASE_URL}/${id}`, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res.data;
@@ -28,7 +29,7 @@ export async function deleteMovie(id) {
 
 export async function updateMovie(id, data) {
   const token = localStorage.getItem("token");
-  const res = await axios.put(`${API_URL}/${id}`, data, {
+  const res = await axios.put(`${BASE_URL}/${id}`, data, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return res.data;
@@ -36,11 +37,9 @@ export async function updateMovie(id, data) {
 
 export async function fetchRecommendedMovies() {
   const token = localStorage.getItem("token");
-  const res = await axios.get(`${API_URL}/recommend`, {
+  const res = await axios.get(`${BASE_URL}/recommend`, {
     headers: { Authorization: `Bearer ${token}` },
   });
   console.log(res.data.recommendations);
   return res.data.recommendations;
 }
-
-
